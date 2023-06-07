23f81e4ea914f1194c909b9dcbda6fb5653af75a
Njunji <samwelnjunji@gmail.com>
2023-06-07 23:45:33 +0300

    First Commit

---
+++ src/main/java/com/codechamp/redisN/RedisNApplication.java
@@ -0,0 +1,13 @@
+package com.codechamp.redisN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechamp/redisN/RedisNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechamp.redisN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
